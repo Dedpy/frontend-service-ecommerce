965457dc7033a89f53e2190dff8bf61e0c6ccdbd
Dedpy <wassimbenr@gmail.com>
2023-11-05 00:08:51 +0100

    feat: adding toast for notification$

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import Produits from "./pages/produits/Produits";
@@ -7,20 +7,23 @@ import Payment from "./pages/payment/payment";
 import SignIn from "./pages/auth/signin/signin";
 import SignUp from "./pages/auth/signup/signup";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const [token, setToken] = useState();
   return (
     <main className="min-h-screen text-center bg-background text-primary ">
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn setToken={setToken} />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/produits" element={<Produits />} />
           <Route path="/panier" element={<Panier />} />
           <Route path="/payment" element={<Payment />} />
         </Routes>
+        <Toaster position="bottom-center" reverseOrder={false} />
       </Router>
     </main>
   );
--- src/pages/auth/signin/signin.tsx
+++ src/pages/auth/signin/signin.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 
-function SignIn() {
+async function loginUser(username: string, password: string) {
+  return fetch("http://localhost:3001/auth/sign-in", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  }).then((data) => {
+    if (!data.ok) {
+      throw new Error("Failed to log in. Please check your credentials.");
+    }
+    return data.json();
+  });
+}
+
+function SignIn({ setToken }: { setToken: any }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const token = await loginUser(username, password);
+      setToken(token);
+    } catch (error) {
+      toast.error("Error while logging in"); // Set the error message
+    }
+  };
+
   return (
     <section>
       <div className="flex items-center justify-center p-4">
@@ -10,17 +39,18 @@ function SignIn() {
             <h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label className="block mb-2 text-sm font-medium  text-white">
-                  Your email
+                  Your username
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="username"
+                  name="username"
+                  id="username"
                   className=" border   sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="name@company.com"
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <div>
@@ -33,6 +63,7 @@ function SignIn() {
                   id="password"
                   placeholder="••••••••"
                   className=" border   sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className="flex items-center justify-between">
